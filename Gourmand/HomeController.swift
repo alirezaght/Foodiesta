f917815fe29901a162b6b4f1d8dd81a284b24770
MacMini <macmini@macminis-mac-mini.local>
2016-03-07 14:15:06 +0330

    ///

--- Gourmand/HomeController.swift
+++ Gourmand/HomeController.swift
@@ -8,9 +8,15 @@
 
 import Foundation
 
-class HomeController : UITableViewController {
+class HomeController : UIViewController {
+   
+    @IBOutlet weak var lblFoodiesta: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+//        lblFoodiesta.layer.borderWidth = 1.0
+//        lblFoodiesta.layer.borderColor =  UIColor( red: 60/255, green: 60/255, blue: 60/255, alpha: 0.6 ).CGColor
     }
 
 }
